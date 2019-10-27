9967dc68dd3b391f613be3dd12bc682c817a2df8
Jacob <jwande18@asu.edu>
2019-10-27 15:35:51 -0700

    added point and collision system revamped the scene changed the direction to fit with the directional light added collision boxes to each cardboard cutout

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
 	public int points = 0;
+	public Text pointsDisp;
 	
 	public void addPoints(int value) {
 		points += value;
@@ -18,6 +21,6 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        pointsDisp.text = "Points: " + points;
     }
 }
---
+++ Assets/Scripts/PunchCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchCollision : MonoBehaviour
+{		
+	void OnCollisionEnter(Collision collision) {
+		if(collision.gameObject.tag == "PropTag") {
+			Debug.Log("Collided with Prop!");
+			DestroyObject(collision.gameObject);
+			FindObjectOfType<GameManager>().addPoints(1);
+		}
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
